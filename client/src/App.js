e56ae8be13e32a844effe5a47bf207fc3e80120b
Germán Mora <dmm71317@gmail.com>
2024-01-20 19:09:34 -0600

    Modificaciones a App.js en react 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import CharacterDetail from './pages/CharacterDetail';
+import CharacterDetail from './pages/CharacterDetail.js';
 
 const App = () => {
   return (
