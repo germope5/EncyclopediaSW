1c59827c4e591b17ed67f4f2a7016e3e2a66546d
Germán Mora <dmm71317@gmail.com>
2024-01-22 16:09:34 -0600

    Añadir enlaces al Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,6 +8,8 @@ const Footer = () => {
   return (
     <footer className="footer-style">
       <p>&copy; {currentYear} Derechos Reservados | Creado por {creatorName}</p>
+      <a href='https://swapi.dev/' className='enlaceSWAPI' target="_blank"><p className='swapi-title'>Comprobar SWAPI</p></a>
+      <a href='mailto:dmm71317@gmail.com' className='enlaceCon' target="_blank"><p className='contacto-title'>¡Contáctame!</p></a>
     </footer>
   );
 };
--- client/src/pages/CharacterDetails.js
+++ client/src/pages/CharacterDetails.js
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getCharacterById } from '../services/swapiService.js';
 import '../../../client/src/App.css';
-import {charactersWithImages} from '../../src/pages/Home.js';
+
 
 const CharacterDetail = () => {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
 
+  // Función para obtener el índice del personaje basado en su URL
+  const getCharacterIndex = (character) => {
+    const urlParts = character.url.split('/');
+    return urlParts[urlParts.length - 2]; // El índice se encuentra antes del último segmento en la URL
+  };
+
   useEffect(() => {
     const fetchCharacterDetail = async () => {
       try {
         const response = await getCharacterById(id);
-        setCharacter(response);
+        setCharacter({
+          ...response,
+          image_url: `${process.env.PUBLIC_URL}/static/img/people/${getCharacterIndex(response)}.jpg`,
+        });
       } catch (error) {
         console.error('Error fetching character detail:', error);
       }
@@ -23,15 +32,15 @@ const CharacterDetail = () => {
 
   return (
     <div className='CharacterDetails'>
-      <h2>Detalles del Personaje</h2>
+      <h2 className='cd-title'>Detalles del Personaje</h2>
       {character && (
         <div>
-          <h3>{character.name}</h3>
-          <img src={character.image_url} alt={character.name} />
-          <p>Height: {character.height}</p>
-          <p>Gender: {character.gender}</p>
-          <p>Birth Year: {character.birth_year}</p>
-          <p>Mass: {character.mass}</p>
+          <h1>{character.name}</h1>
+          <img className='characterImagen' src={character.image_url} alt={character.name} />
+          <p><b>Height:</b> {character.height}</p>
+          <p><b>Gender: </b>{character.gender}</p>
+          <p><b>Birth Year:</b> {character.birth_year}</p>
+          <p><b>Mass:</b>Mass: {character.mass}</p>
           {/* Add more details as needed */}
         </div>
       )}
