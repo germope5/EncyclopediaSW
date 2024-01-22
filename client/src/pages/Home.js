d563c449299cde10afbaaa52cee82b7d2a4e99e3
Germán Mora <dmm71317@gmail.com>
2024-01-22 15:20:33 -0600

    Solución a imágenes Paginación

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,36 +7,40 @@ import '../../../client/src/App.css';
 
 const Home = () => {
   const ITEMS_PER_PAGE = 10;
-const [characters, setCharacters] = useState([]);
-const [searchTerm, setSearchTerm] = useState('');
-const [page, setPage] = useState(1);
-const [totalPages, setTotalPages] = useState(0);
+  const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-useEffect(() => {
-  
-  const fetchCharacters = async () => {
-    try {
-      const response = searchTerm
-        ? await searchCharacters(searchTerm)
-        : await getCharacters(page);
-
-      // Supongamos que tu API devuelve objetos con propiedades 'name' y 'url'
-      // Agregamos una propiedad 'image' que contiene la URL de la imagen local
-      const charactersWithImages = response.results.map((character, index) => ({
-        ...character,
-        image_url: `${process.env.PUBLIC_URL}/static/img/people/${index + 1}.jpg`,
-
-      }));
-
-      setCharacters(charactersWithImages);
-      setTotalPages(Math.ceil(response.count / ITEMS_PER_PAGE));
-    } catch (error) {
-      console.error('Error fetching characters:', error);
-    }
+   // Función para obtener el índice del personaje basado en su URL
+   const getCharacterIndex = (character) => {
+    const urlParts = character.url.split('/');
+    return urlParts[urlParts.length - 2]; // El índice se encuentra antes del último segmento en la URL
   };
 
-  fetchCharacters();
-}, [page, searchTerm]);
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = searchTerm
+          ? await searchCharacters(searchTerm)
+          : await getCharacters(page);
+
+          const charactersWithImages = response.results.map((character) => ({
+            ...character,
+            image_url: `${process.env.PUBLIC_URL}/static/img/people/${getCharacterIndex(character)}.jpg`,
+          }));
+          
+         
+
+        setCharacters(charactersWithImages);
+        setTotalPages(Math.ceil(response.count / ITEMS_PER_PAGE));
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    };
+
+    fetchCharacters();
+  }, [page, searchTerm]);
 
   const handleSearchChange = (event) => {
     const term = event.target.value.toLowerCase();
@@ -57,7 +61,7 @@ useEffect(() => {
             <li key={character.name} className='character-item'>
               <Link to={`/characters/${character.url.split('/').reverse()[1]}`}>
                 <img className='img-character' src={character.image_url} alt={character.name} />
-                <span className='.character-name'>{character.name}</span>
+                <span className='character-name'>{character.name}</span>
               </Link>
             </li>
           ))}
@@ -65,9 +69,7 @@ useEffect(() => {
         <Pagination page={page} totalPages={totalPages} onChange={handlePageChange} />
       </div>
     </div>
-    
   );
 };
 
 export default Home;
-
